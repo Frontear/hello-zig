cb5791d52492501634900b2336303f2381e148fc
Ali Rizvi <perm-iterate-0b@icloud.com>
2023-12-24 16:33:57 -0500

    learn: @import, Error Union Type, try, tuples Pretty interesting stuff, the main thing I find unique here is that @import() essentially returns an anonymous structure rather than polluting the global namespace. This is different from C since a #include would simply plop the exact code on top, resulting in possible name clashes. You can even pull only specific things from the @import(), which is awesome. Tuples are a bit strange, I didn't expect the dot and use of curly braces, but I don't think it would be terribly hard to get used to, though im curious the reason for why the dot is necessary in the first place. I like that error handling is baked into the syntax, and I can see the Rust inspirations from this too. Try is interesting, but I'm more curious about different error handling approaches because this to me just screams .unwrap() from Rust, whereas I'd prefer a match() => { Ok, Err } type thing instead. We'll see what other styles of handling exist.

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,33 @@
+// @import will create a structure that contains the functions variables etcs of the "std" (standard) library.
+// because it creates a structure, its necessary to save it into a variable, which we do with const std = ...;
+// const obviously means constant, unchanging, and by doing all this we can then access functions and other things
+// from the standard library via std.NAME_OF_THING
+//
+// In total: This is a immutable constant variable called std that imports an unnamed structure containing all of the standard library functions.
 const std = @import("std");
 
+// pub fn means public function. main() is just the entrypoint, like most other languages
+// !void has two separate meanings, its called an Error Union Type
+// the above essentially means that it will either return an Error (defined by the !),
+// OR, it will return a type void (meaning nothing, as most other languages have it too)
+// The full form is supposed to be <errortype>!<rettype>, but here we don't explicitly state error type
+// Zig will set its type to an inferred error set type (w/e that means)
+//
+// In total: This is a public function named main, with no parameters, and can return either an error, or nothing at all.
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // Grabs the standard output writer (unsure if this is a copy or a reference to the actual object).
+    // Save it into a constant called stdout
+    const stdout = std.io.getStdOut().writer();
+    // "Tries" to run the print function within stdout, with arguments "Hello, {s}!\n", .{"world"} <-- string substitution
+    // These two arguments are evaluated at COMPILE-time
+    // The .{} is a tuple structure
+    // The "try" part will run the code, and if it returns an error (a function with !<rettype>),
+    // it'll return the function at that exact statement with the error
+    // e.g. if print() returned an error, it would stop code execution, and return the calling function main()
+    // with the error from stdout.print()
+    // Note: this all implies that in order to run a try, you NEED to mark your calling function return type with !
+    // There's probably other error handling that does not need to pass it back to the calling function, but idk those yet.
+    //
+    // In total: Try only applies to return types that are marked with !, it will run the code and if it returns an error, stops the function and returns that exact error
+    try stdout.print("Hello, {s}!\n", .{"world"});
 }
